e084c9509c7ca243ba8bfde97690667148436dd9
Eric Dunston <eric.dunston@siriusinsight.ai>
2022-02-16 14:04:30 +0100

    add time description custom action

--- actions/actions.py
+++ actions/actions.py
@@ -8,6 +8,8 @@
 # This is a simple example for a custom action which utters "Hello World!"
 
 from typing import Any, Text, Dict, List
+from datetime import datetime as dt
+
 
 from rasa_sdk import FormValidationAction, Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
@@ -36,6 +38,26 @@ class ValidateActivityForm(FormValidationAction):
             dispatcher.utter_message(text=f"OK! You have a slot booked in your calender at\
                  {slot_value}")
             return{"time": slot_value}
+
+from typing import Text, Dict, Any, List
+from rasa_sdk import Action
+from rasa_sdk.events import SlotSet
+
+class ActionSaveTimeDescription(Action):
+   def name(self) -> Text:
+      return "action_get_description_time"
+
+   def run(self,
+           dispatcher: CollectingDispatcher,
+           tracker: Tracker,
+           domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+      time = tracker.get_slot('time')
+      time_object = dt.strptime(time, "%Y-%m-%dT%H:%M:%S.%f%z")
+      out_str = time_object.strftime('%b %d %Y %I:%M%p')
+
+
+      return [SlotSet("description_time", out_str if out_str is not None else [])]
 
 
 
