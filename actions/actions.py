eb7bfb4bd896b3f5e0ef422a170cf26afd6c2c56
Eric Dunston <eric.dunston@siriusinsight.ai>
2022-02-16 10:50:27 +0100

    add activity form

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,36 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import FormValidationAction, Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import EventType
+from rasa_sdk.types import DomainDict
+
+
+class ValidateActivityForm(FormValidationAction):
+
+    def name(self) -> Text:
+        return "validate_activity_form"
+    
+    def validate_activity(self, slot_value: Any, 
+            dispatcher: CollectingDispatcher, 
+            tracker: Tracker, domain: DomainDict) -> Dict[Text,Any]: 
+        ### Check if date is in the future.
+        if slot_value :
+            dispatcher.utter_message(text=f"OK! You are going to {slot_value}")
+            return{"activity": slot_value}
+    
+    def validate_time(self, slot_value: Any, 
+            dispatcher: CollectingDispatcher, 
+            tracker: Tracker, domain: DomainDict) -> Dict[Text,Any]: 
+        ### Check if date is in the future.
+        if slot_value :
+            dispatcher.utter_message(text=f"OK! You have a slot booked in your calender at\
+                 {slot_value}")
+            return{"time": slot_value}
+
+
+
+     
